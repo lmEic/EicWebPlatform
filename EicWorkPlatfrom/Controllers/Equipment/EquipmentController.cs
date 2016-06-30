0ce3e2371bb5eb103308747b8813e38d4df5581e
eicmain <ylei@ezconn.cn>
2016-06-30 10:47:24 +0800

    add export to excel

--- EicWorkPlatfrom/Controllers/Equipment/EquipmentController.cs
+++ EicWorkPlatfrom/Controllers/Equipment/EquipmentController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace EicWorkPlatfrom.Controllers
 {
     public class EquipmentController : EicBaseController
@@ -16,5 +17,27 @@ namespace EicWorkPlatfrom.Controllers
             return View();
         }
 
+        public ActionResult AstArchiveInput()
+        {
+            return View();
+        }
+
+
+        public FileResult ExportToExcel()
+        {
+            List<DDD> datas = new List<DDD>() { 
+               new DDD(){ Name="aaa", Age =20},
+               new DDD(){ Name="bbb", Age =25},
+            };
+
+            return this.ExportToExcel<DDD>(datas, "aaa", "AAA");
+        }
+    }
+
+    public class DDD
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
     }
 }
--- Lm.Eic.Uti.Common/Lm.Eic.Uti.Common/YleeExtension/FileOperation/FileOperationExtension.cs
+++ Lm.Eic.Uti.Common/Lm.Eic.Uti.Common/YleeExtension/FileOperation/FileOperationExtension.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System.Reflection;
 namespace Lm.Eic.Uti.Common.YleeExtension.FileOperation
 {
    public static class FileOperationExtension
@@ -130,6 +133,135 @@ namespace Lm.Eic.Uti.Common.YleeExtension.FileOperation
        public static bool ExistFile(this string filePath)
        {
            return File.Exists(filePath);
+       }
+
+
+       /// <summary>
+       /// 扩展方法：使用NPOI将数据导入到Excel内存流
+       /// </summary>
+       /// <typeparam name="T">数据类型</typeparam>
+       /// <param name="dataSource">数据集合</param>
+       /// <param name="xlsSheetName">Sheet名称</param>
+       /// <returns></returns>
+       public static MemoryStream ExportToExcel<T>(this List<T> dataSource, string xlsSheetName) where T : class
+       {
+           MemoryStream stream = new MemoryStream();
+           if (dataSource == null || dataSource.Count == 0) return stream;
+
+           HSSFWorkbook workbook = new HSSFWorkbook();
+           ISheet sheet = workbook.CreateSheet(xlsSheetName);
+           ICellStyle cellSytleDate = workbook.CreateCellStyle();
+           IDataFormat format = workbook.CreateDataFormat();
+           cellSytleDate.DataFormat = format.GetFormat("yyyy年mm月dd日");
+
+           try
+           {
+               #region 填充列头区域
+               Type t = dataSource[0].GetType();
+               PropertyInfo[] pis = t.GetProperties();
+               IRow rowHeader = sheet.CreateRow(0);
+               //设置表头样式
+               ICellStyle headStyle = workbook.CreateCellStyle();
+               headStyle.Alignment = HorizontalAlignment.Center;
+               IFont cellFontHeader = workbook.CreateFont();
+               cellFontHeader.Boldweight = 700;
+               cellFontHeader.FontHeightInPoints = 12;
+               headStyle.SetFont(cellFontHeader);
+
+               for (int colIndex = 0; colIndex < pis.Length; colIndex++)
+               {
+                   ICell cell = rowHeader.CreateCell(colIndex);
+                   cell.SetCellValue(pis[colIndex].Name);
+                   cell.CellStyle = headStyle;
+               }
+               #endregion
+
+               #region 填充内容区域
+               for (int rowIndex = 0; rowIndex < dataSource.Count; rowIndex++)
+               {
+                   IRow rowContent = sheet.CreateRow(rowIndex + 1);
+                   T entity = dataSource[rowIndex];
+                   Type tentity = entity.GetType();
+                   PropertyInfo[] tpis = tentity.GetProperties();
+                   for (int colIndex = 0; colIndex < tpis.Length; colIndex++)
+                   {
+                       ICell cellContent = rowContent.CreateCell(colIndex);
+                       object value = tpis[colIndex].GetValue(entity, null);
+                       if (value == null) value = "";
+                       Type type = value.GetType();
+                       switch (type.ToString())
+                       {
+                           case "System.String"://字符串类型
+                               cellContent.SetCellValue(value.ToString());
+                               break;
+                           case "System.DateTime"://日期类型
+                               DateTime dateV;
+                               DateTime.TryParse(((DateTime)value).ToString("yyyy-MM-dd HH:mm"), out dateV);
+                               cellContent.SetCellValue(dateV);
+                               cellContent.CellStyle = cellSytleDate;//格式化显示数据
+                               break;
+                           case "System.Boolean"://布尔型
+                               bool boolV = false;
+                               if (bool.TryParse(value.ToString(), out boolV))
+                                   cellContent.SetCellValue(boolV);
+                               else
+                                   cellContent.SetCellValue("解析布尔型数据错误");
+                               break;
+                           case "System.Int16"://整型
+                           case "System.Int32":
+                           case "System.Int64":
+                           case "System.Byte":
+                               int intV = 0;
+                               if (int.TryParse(value.ToString(), out intV))
+                                   cellContent.SetCellValue(intV);
+                               else
+                                   cellContent.SetCellValue("解析整型数据错误");
+                               break;
+                           case "System.Decimal"://浮点型
+                           case "System.Double":
+                               double doubV = 0;
+                               if (double.TryParse(value.ToString(), out doubV))
+                                   cellContent.SetCellValue(doubV);
+                               else
+                                   cellContent.SetCellValue("解析浮点型或双精度型数据错误");
+                               break;
+                           case "System.DBNull"://空值处理
+                               cellContent.SetCellValue("");
+                               break;
+                           default:
+                               cellContent.SetCellValue("");
+                               break;
+                       }
+                   }
+               }
+               #endregion
+
+               sheet.ForceFormulaRecalculation = true;
+               workbook.Write(stream);
+               return stream;
+           }
+           catch (Exception ex)
+           {
+               throw new Exception(ex.ToString());
+           }
+       }
+
+       /// <summary>
+       /// 扩展方法：导入到现有的Excel模板文件中
+       /// </summary>
+       public static void ExportToExcelTemplateFile(this string templateFilePath, string xlsSheetName, string saveFileName, Action<ISheet> setDataToSheetHandler)
+       {
+           FileStream fs = new FileStream(templateFilePath, FileMode.Open, FileAccess.Read);//FileAccess.Read 保证文件不被占用
+           HSSFWorkbook workbook = new HSSFWorkbook(fs);
+           ISheet sheet = workbook.GetSheet(xlsSheetName);
+           if (setDataToSheetHandler != null)
+           {
+               setDataToSheetHandler(sheet);
+               sheet.ForceFormulaRecalculation = true;//强制要求Excel在打开时重新计算的属性，在拥有公式的xls文件中十分有用
+           }
+           FileStream localFile = new FileStream(saveFileName, FileMode.Create);
+           workbook.Write(localFile);
+           localFile.Close();
        }
     }
 }
