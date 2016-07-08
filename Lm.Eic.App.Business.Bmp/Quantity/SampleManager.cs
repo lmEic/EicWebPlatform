76e53c318d778f708354ef8e0a73a2422942207d
xiaoqiao2016 <wxq520@ezconn.cn>
2016-07-09 07:58:28 +0800

    添加 增 删 改

--- Lm.Eic.App.Business.Bmp/Quantity/SampleManager.cs
+++ Lm.Eic.App.Business.Bmp/Quantity/SampleManager.cs
@@ -7,6 +7,7 @@ using Lm.Eic.App.DomainModel.Bpm.Quanity;
 using Lm.Eic.App.DbAccess.Bpm.Repository.QuantityRep;
 using Lm.Eic.App.Erp.Bussiness.QuantityManage;
 using Lm.Eic.App.Erp.Domain .QuantityModel;
+using Lm.Eic.Uti.Common.YleeOOMapper;
 
 namespace Lm.Eic.App.Business.Bmp.Quantity
 {
@@ -41,8 +42,44 @@ namespace Lm.Eic.App.Business.Bmp.Quantity
         {
             return irep.Entities.Where(e => e.OrderID == Orderid & e.SampleMaterial == SampleMaterial).ToList();
         }
-      
-        
+
+
+        public OpResult Store(List<IQCSampleItemRecordModel> listModel)
+        {
+            try
+            {
+                if (listModel == null || listModel.Count <= 0)
+                    return OpResult.SetResult("集合不能为空！", false);
+
+                int record = 0;
+                string opSign = string.Empty;
+                opSign = listModel[0].OpSign;
+
+                switch (opSign)
+                {
+                    case OpMode.Add: //新增
+                        record = 0;
+                        listModel.ForEach(model => { record += irep.Insert(model); });
+                        return OpResult.SetResult("添加成功！", "添加失败！", record);
+
+                    case OpMode.Edit: //修改
+                        record = 0;
+                        listModel.ForEach(model => { record += irep.Update(u => u.Id_key  == model.Id_key, model); });
+                        return OpResult.SetResult("更新成功！", "更新失败！", record);
+
+                    case OpMode.Delete: //删除
+                        record = 0;
+                        listModel.ForEach(model => { record += irep.Delete(model.Id_key); });
+                        return OpResult.SetResult("删除成功！", "删除失败！", record);
+
+                    default: return OpResult.SetResult("操作模式溢出！", false);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.InnerException.Message);
+            }
+        }
         
         /// <summary>
         /// 得到抽样物料信息 （单头）
