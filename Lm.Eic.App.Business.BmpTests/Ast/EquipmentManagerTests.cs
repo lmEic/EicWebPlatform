9371a15af46c3fa6620fb4a636298a6e79a27f41
fengyie55 <fengyie55@hotmail.com>
2016-07-19 14:49:49 +0800

    修改同步

--- Lm.Eic.App.Business.BmpTests/Ast/EquipmentManagerTests.cs
+++ Lm.Eic.App.Business.BmpTests/Ast/EquipmentManagerTests.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Lm.Eic.Uti.Common.YleeExtension.Conversion;
 
 namespace Lm.Eic.App.Business.Bmp.Ast.Tests
 {
@@ -15,20 +16,38 @@ namespace Lm.Eic.App.Business.Bmp.Ast.Tests
         public void BuildAssetNumberTest()
         {
             EquipmentManager tem2 = new EquipmentManager();
-           
+
             Lm.Eic.App.DomainModel.Bpm.Ast.EquipmentModel model = new DomainModel.Bpm.Ast.EquipmentModel();
             model.AssetNumber = "I169001";
             model.EquipmentName = "Test1";
-           // model.Id_Key = 4;
+            // model.Id_Key = 4;
 
-             //修改
+            //修改
             //var ttt = tem.ChangeStorage(model, 1);
 
             string i = tem2.BuildAssetNumber("生产设备", "低值易耗品", "保税");
             string i2 = tem2.BuildAssetNumber("生产设备", "低值易耗品", "非保税");
             string i3 = tem2.BuildAssetNumber("生产设备", "固定资产", "保税");
-          
-         //  Assert.Fail();
+
+            //  Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StoreTest()
+        {
+            DomainModel.Bpm.Ast.EquipmentModel model = new DomainModel.Bpm.Ast.EquipmentModel();
+            model.AssetNumber = AstService.EquipmentManager.BuildAssetNumber("生产设备", "低值易耗品", "保税");
+            model.EquipmentType = "生产设备";
+            model.AssetNumber = "低值易耗品";
+            model.TaxType = "保税";
+            model.EquipmentName = "Test";
+            model.MaintenanceDate = DateTime.Now.ToDate();
+            model.MaintenanceInterval = 10;
+            model.CheckDate = DateTime.Now.ToDate();
+            model.CheckInterval = 6;
+            model.OpSign = "add";
+          var tem =  AstService.EquipmentManager.Store(model);
+            Assert.Fail();
         }
     }
 }
