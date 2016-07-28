d96a9119554f9bb663909fedb0fe8b1c8c0c198d
fengyie55 <fengyie55@163.com>
2016-07-28 11:17:36 +0800

    Change Equipment

--- Lm.Eic.App.Business.Bmp/Ast/EquipmentMaintenanceManager.cs
+++ Lm.Eic.App.Business.Bmp/Ast/EquipmentMaintenanceManager.cs
@@ -45,14 +45,20 @@ namespace Lm.Eic.App.Business.Bmp.Ast
         /// </summary>
         /// <param name="qryDto">设备查询数据传输对象 </param>
         /// <returns></returns>
-        public List<EquipmentMaintenanceModel> FindBy(QueryEquipmentDto qryDto) { return CrudFactory.EquipmentMaintenanceCrud.FindBy(qryDto); }
+        public List<EquipmentMaintenanceModel> FindBy(QueryEquipmentDto qryDto)
+        {
+            return CrudFactory.EquipmentMaintenanceCrud.FindBy(qryDto);
+        }
 
         /// <summary>
         /// 修改数据仓库 PS：model.OpSign = add/edit/delete
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
-        public OpResult Store(EquipmentMaintenanceModel model) { return CrudFactory.EquipmentMaintenanceCrud.Store(model); }
+        public OpResult Store(EquipmentMaintenanceModel model)
+        {
+            return CrudFactory.EquipmentMaintenanceCrud.Store(model);
+        }
 
     }
 }
