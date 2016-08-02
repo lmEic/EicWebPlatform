3cadf2c2ae77efc2c9cdac2744896811176f8970
ZhangMing <fengyie55@hotmail.com>
2016-08-02 14:18:01 +0800

    change

--- Lm.Eic.App.Business.Bmp/Ast/EquipmentCrud.cs
+++ Lm.Eic.App.Business.Bmp/Ast/EquipmentCrud.cs
@@ -368,6 +368,7 @@ namespace Lm.Eic.App.Business.Bmp.Ast
                     {
                         case OpMode.Add: //新增
                             opResult = irep.Insert(model).ToOpResult_Add(opContext, model.Id_Key);
+                            opResult.Attach = model;
                             break;
                         case OpMode.Edit: //修改
                             opResult = irep.Update(u => u.Id_Key == model.Id_Key, model).ToOpResult_Eidt(opContext);
