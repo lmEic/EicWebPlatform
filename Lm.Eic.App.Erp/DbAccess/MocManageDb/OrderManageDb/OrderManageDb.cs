5635a6b5ad92f778044bdcaf847b7d54f0e93aff
ZhangMing <fengyie55@hotmail.com>
2016-08-16 11:40:09 +0800

    Add  OrderMange to Erp

---
+++ Lm.Eic.App.Erp/DbAccess/MocManageDb/OrderManageDb/OrderManageDb.cs
@@ -0,0 +1,35 @@
+﻿using Lm.Eic.App.Erp.Domain.MocManageModel.OrderManageModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lm.Eic.App.Erp.DbAccess.MocManageDb.OrderManageDb
+{
+    public class OrderDetailsDb
+    {
+        /// <summary>
+        /// 获取工单详情
+        /// </summary>
+        /// <param name="orderID"></param>
+        /// <returns></returns>
+        public OrderModel GetOrderDetailsBy(string orderID)
+        {
+            return null;
+        }
+    }
+
+    public class OrderMaterialDb
+    {
+        /// <summary>
+        /// 获取工单物料列表
+        /// </summary>
+        /// <param name="orderId"></param>
+        /// <returns></returns>
+        public List<MaterialModel> GetOrderMaterialListBy(string orderId)
+        {
+            return null;
+        }
+
+    }
+}
---
+++ Lm.Eic.App.Erp/Domain/MocManageModel/OrderManageModel/MaterialModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lm.Eic.App.Erp.Domain.MocManageModel.OrderManageModel
+{
+    /// <summary>
+    /// 工单物料
+    /// </summary>
+    public class MaterialModel
+    {
+        //ToDo:料号，品名，规格，单位，需领用量，已领用量，等......
+    }
+}
