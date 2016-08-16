48045a2761ab01d9e94d82d711c4e2e8c9add51c
ZhangMing <fengyie55@hotmail.com>
2016-08-16 17:11:37 +0800

    Change OrderManage

--- Lm.Eic.App.Erp/Domain/MocManageModel/OrderManageModel/MaterialModel.cs
+++ Lm.Eic.App.Erp/Domain/MocManageModel/OrderManageModel/MaterialModel.cs
@@ -10,54 +10,29 @@ namespace Lm.Eic.App.Erp.Domain.MocManageModel.OrderManageModel
     /// </summary>
     public class MaterialModel
     {
-        //ToDo:料号，品名，规格，单位，需领用量，已领用量，等......
         /// <summary>
         ///  料号
         /// </summary>
-        public string MaterialId
-        {
-            set;
-            get;
-        }
+        public string MaterialId { set; get; }
         /// <summary>
-        ///   品名
+        ///  品名
         /// </summary>
-        public string MaterialName
-        {
-            set;
-            get;
-        }
+        public string MaterialName { set; get; }
         /// <summary>
-        ///   规格
+        ///  规格
         /// </summary>
-        public string MaterialSpecify
-        {
-            set;
-            get;
-        }
+        public string MaterialSpecify { set; get; }
         /// <summary>
-        ///    单位
+        ///  单位
         /// </summary>
-        public string MaterialUnit
-        { 
-            set; 
-            get; 
-        }
+        public string Unit { set; get; }
         /// <summary>
-        ///    需领用量
+        ///  需领用量
         /// </summary>
-        public double MaterialNeedCount
-        {
-            set;
-            get;
-        }
+        public double NeedCount { set; get; }
         /// <summary>
-        ///      已领用量
+        ///  已领用量
         /// </summary>
-        public double MaterialReceiveCount
-        {
-            set;
-            get;
-        }
+        public double ReceiveCount { set; get; }
     }
 }
