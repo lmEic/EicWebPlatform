08203636907deebc43080569e6fa62944473e3af
eicmain <ylei@ezconn.cn>
2016-08-27 17:12:49 +0800

    Done

--- Lm.Eic.Uti.Common.YleeDbHandler/Lm.Eic.Uti.Common.YleeDbHandler/CrudBase.cs
+++ Lm.Eic.Uti.Common.YleeDbHandler/Lm.Eic.Uti.Common.YleeDbHandler/CrudBase.cs
@@ -63,13 +63,24 @@ namespace Lm.Eic.Uti.Common.YleeDbHandler
         {
             return this.PersistentDatas(model);
         }
+        /// <summary>
+        /// 设定固定字段值
+        /// </summary>
+        /// <param name="entity"></param>
+        protected virtual void SetFixFieldValue(TEntity entity)
+        {
+            var optimePi = IsHasProperty(entity, "OpTime");
+            if (optimePi != null) optimePi.SetValue(entity, DateTime.Now.ToDateTime(), null);
+            var opDatePi = IsHasProperty(entity, "OpDate");
+            if (opDatePi != null) opDatePi.SetValue(entity, DateTime.Now.ToDate(), null);
+        }
 
         /// <summary>
         /// 持久化数据
         /// </summary>
         /// <param name="entity"></param>
         /// <returns></returns>
-        public OpResult PersistentDatas(TEntity entity)
+        protected OpResult PersistentDatas(TEntity entity)
         {
             OpResult result = OpResult.SetResult("持久化数据操作失败!");
             string opSign = "default";
@@ -81,11 +92,7 @@ namespace Lm.Eic.Uti.Common.YleeDbHandler
             {
                 //基本属性赋值
                 if (entity == null) return OpResult.SetResult("entity can't set null!");
-                var optimePi = IsHasProperty(entity, "OpTime");
-                if (optimePi != null) optimePi.SetValue(entity, DateTime.Now.ToDateTime(), null);
-                var opDatePi = IsHasProperty(entity, "OpDate");
-                if (opDatePi != null) opDatePi.SetValue(entity, DateTime.Now.ToDate(), null);
-
+                SetFixFieldValue(entity);
                 //取得操作方法
                 PropertyInfo pi = IsHasProperty(entity, "OpSign");
                 if (pi == null)
@@ -103,75 +110,6 @@ namespace Lm.Eic.Uti.Common.YleeDbHandler
             catch (Exception ex) { throw new Exception(ex.InnerException.Message); }
             return result;
         }
-
-        /// <summary>
-        /// 持久化数据
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="addfn"></param>
-        /// <param name="editfn"></param>
-        /// <param name="delfn"></param>
-        /// <returns></returns>
-        protected OpResult PersistentDatas(TEntity entity, Func<TEntity, OpResult> addfn, Func<TEntity, OpResult> editfn, Func<TEntity, OpResult> delfn)
-        {
-           
-            OpResult result = OpResult.SetResult("持久化数据操作失败!");
-            string opSign = "default";
-            try
-            {
-                if (entity == null) return OpResult.SetResult("entity can't set null!");
-                var optimePi = IsHasProperty(entity, "OpTime");
-                if (optimePi != null) optimePi.SetValue(entity, DateTime.Now.ToDateTime(), null);
-                var opDatePi = IsHasProperty(entity, "OpDate");
-                if (opDatePi != null) opDatePi.SetValue(entity, DateTime.Now.ToDate(), null);
-
-                PropertyInfo pi = IsHasProperty(entity, "OpSign");
-                if (pi != null) opSign = pi.GetValue(entity, null) as string;
-                switch (opSign)
-                {
-                    case OpMode.Add:
-                        result = addfn(entity);
-                        break;
-                    case OpMode.Edit:
-                        result = editfn(entity);
-                        break;
-                    case OpMode.Delete:
-                        result = delfn(entity);
-                        break;
-                    default:
-                        break;
-                }
-
-            }
-            catch (Exception ex) {throw new Exception(ex.InnerException.Message);}
-            return result;
-        }
-        /// <summary>
-        /// 持久化数据
-        /// </summary>
-        /// <param name="entity"></param>
-        /// <param name="addfn"></param>
-        /// <param name="editfn"></param>
-        /// <returns></returns>
-        protected OpResult PersistentDatas(TEntity entity, Func<TEntity, OpResult> addfn, Func<TEntity, OpResult> editfn)
-        {
-            OpResult result = OpResult.SetResult("持久化数据操作失败!");
-            string opSign = "default";
-            PropertyInfo pi = IsHasProperty(entity, "OpSign");
-            if (pi != null) opSign = pi.GetValue(entity, null) as string;
-            switch (opSign)
-            {
-                case OpMode.Add:
-                    result = addfn(entity);
-                    break;
-                case OpMode.Edit:
-                    result = editfn(entity);
-                    break;
-                default:
-                    break;
-            }
-            return result;
-        }
         /// <summary>
         /// 模型是否含有该属性
         /// </summary>
@@ -184,8 +122,6 @@ namespace Lm.Eic.Uti.Common.YleeDbHandler
             PropertyInfo pi= type.GetProperties().ToList().FirstOrDefault(e => e.Name == propertyName);
             return pi;
         }
-
-    
         protected OpResult StoreEntity(TEntity entity,Func<TEntity,OpResult> storeHandler)
         {
             OpResult result = null;
