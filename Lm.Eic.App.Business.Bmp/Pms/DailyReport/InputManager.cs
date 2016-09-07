3776d079bedbc375a6d7db7414e481eccf654725
ZhangMing <fengyie55@hotmail.com>
2016-09-07 11:53:10 +0800

    Change ProductFlowConfig

---
+++ Lm.Eic.App.Business.Bmp/Pms/DailyReport/InputManager.cs
@@ -0,0 +1,52 @@
+﻿using Lm.Eic.Uti.Common.YleeObjectBuilder;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lm.Eic.App.Business.Bmp.Pms.DailyReport
+{
+    /// <summary>
+    /// 日报输入管理器
+    /// </summary>
+    public class InputManager
+    {
+        /// <summary>
+        /// 日报输入管理器
+        /// </summary>
+        public DailyReportInputManager DailyReportInputManager
+        {
+            get { return OBulider.BuildInstance<DailyReportInputManager>(); }
+        }
+
+        /// <summary>
+        /// 日报模板管理器
+        /// </summary>
+        public DailyReportTemplateManager DailyReportTemplateManager
+        {
+            get { return OBulider.BuildInstance<DailyReportTemplateManager>(); }
+        }
+    }
+
+
+ 
+
+    /// <summary>
+    /// 日报录入管理器
+    /// </summary>
+    public class DailyReportInputManager
+    {
+
+    }
+
+ 
+    /// <summary>
+    /// 日报模板管理器
+    /// </summary>
+    public class DailyReportTemplateManager
+    {
+
+    }
+
+
+}
