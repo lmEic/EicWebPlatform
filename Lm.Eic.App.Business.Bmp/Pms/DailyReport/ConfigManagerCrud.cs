7219e87198aac9e0ff594f20a12e676b7efb10ad
wxq <wanxiaoqiao888@163.com>
2016-09-09 10:51:10 +0800

    change

--- Lm.Eic.App.Business.Bmp/Pms/DailyReport/ConfigManagerCrud.cs
+++ Lm.Eic.App.Business.Bmp/Pms/DailyReport/ConfigManagerCrud.cs
@@ -137,7 +137,7 @@ namespace Lm.Eic.App.Business.Bmp.Pms.DailyReport
         /// <returns></returns>
         private OpResult DeleteProductFlowModel(ProductFlowModel model)
         {
-            return   (model.Id_Key >0)? 
+            return (model.Id_Key >0)? 
                 irep.Delete(u => u.Id_Key == model.Id_Key).ToOpResult_Delete(OpContext)
                 :OpResult.SetResult("未执行任何操作");
         }
