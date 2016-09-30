eeb7386462c775e76061de5c73d6bff5c8eb5b19
wxq <wanxiaoqiao888@163.com>
2016-09-30 11:00:52 +0800

    change

--- Lm.Eic.App.Erp/DbAccess/MocManageDb/OrderManageDb/OrderManageDb.cs
+++ Lm.Eic.App.Erp/DbAccess/MocManageDb/OrderManageDb/OrderManageDb.cs
@@ -1,4 +1,5 @@
 ﻿using Lm.Eic.App.Erp.Domain.MocManageModel.OrderManageModel;
+using Lm.Eic.App.Erp.Domain.ProductTypeMonitorModel;
 using Lm.Eic.Uti.Common.YleeExtension.Conversion;
 using Lm.Eic.Uti.Common.YleeDbHandler;
 using System;
@@ -103,8 +104,12 @@ namespace Lm.Eic.App.Erp.DbAccess.MocManageDb.OrderManageDb
 
 
    public class BusinessOrder
-   {
-       public List<string>MesProductType()
+   { 
+       /// <summary>
+       /// MES产品型号
+       /// </summary>
+       /// <returns></returns>
+       private List<string>MesProductType()
        {
            StringBuilder sb = new StringBuilder();
            sb.Append("SELECT  DISTINCT ProductTypeCommon  ")
@@ -120,11 +125,33 @@ namespace Lm.Eic.App.Erp.DbAccess.MocManageDb.OrderManageDb
                }
            }
            return ProductTypeList;
-
-          
        }
+       /// <summary>
+       /// 未完工业务订单
+       /// </summary>
+       /// <param name="ProductType"></param>
+       /// <returns></returns>
+       public  List<CopOrderModel> GetCopOrderBy (string ProductType)
+       {
+           string SqlFields = "TD001 AS 单别, TD002 AS 单号, TD003 AS 序号, TD004 AS 品号, TD005 AS 品名, TD006 AS 规格, TD007 AS 仓位号,   TD008 AS 计划产量, TD009 AS 已交量";
+           string sqlWhere = string.Format(" where (TD005 like'%{0}%' or TD006 LIKE '%{0}%')and (TD016 = 'N') ", ProductType);
+           return  ErpDbAccessHelper.FindDataBy<CopOrderModel>(SqlFields, sqlWhere, (dr, m) =>
+           {
+               m.Category = dr["单别"].ToString().Trim();
+               m.Code = dr["单号"].ToString().Trim();
+               m.OrderDesc = dr["序号"].ToString().Trim();
 
+               m.ProductID = dr["品号"].ToString().Trim();
+               m.ProductName = dr["品名"].ToString().Trim();
+               m.ProductSpecify = dr["规格"].ToString().Trim();
+               m.WarehouseID = (dr["仓位号"].ToString().Trim());
 
+               m.ProductNumber = dr["计划产量"].ToString().Trim().ToDouble ();
+               m.FinishNumber = dr["已交量"].ToString().Trim().ToDouble();
+           });
+          
+           
+       }
 
    }
 }
