1f7200c8405aec99a80efc4c79d7c036f3e4cc9e
xiaoqiao2016 <wxq520@ezconn.cn>
2016-10-29 10:45:25 +0800

    gfggg

---
+++ Lm.Eic.App.DbAccess.Bpm/Mapping/PurchaseMapping/SupplierMapping.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity.ModelConfiguration;
+using Lm.Eic.App.DomainModel.Bpm.Purchase;
+
+namespace Lm.Eic.App.DbAccess.Bpm.Mapping.PurchaseMapping
+{
+   /// <summary>
+  ///QualifiedSupplierModelMapping   545454
+  /// </summary>
+  public class QualifiedSupplierModelMapping : EntityTypeConfiguration<QualifiedSupplierModel>
+{
+    public QualifiedSupplierModelMapping()
+    { 
+        this.HasKey(t => t.Id_key);
+        this.Property(t => t.Id_key ).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+        this.ToTable("Pur_QualifiedSupplier");
+    }
+}
+ 
+    /// <summary>
+  /// SupplierEligibleMapping
+  /// </summary>
+  public class SupplierEligibleMapping : EntityTypeConfiguration<SupplierEligibleModel>
+    {
+       public SupplierEligibleMapping()
+        {
+            this.HasKey(t => t.Id_key );
+            this.Property(t => t.Id_key ).HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+            this.ToTable("Pur_SupplierEligible");
+        }
+    }
+}
