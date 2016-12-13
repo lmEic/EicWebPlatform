7c7806976248b3c77b796b6297833dad3d695d0b
xiaoqiao <wanxiaoqiao888@163.com>
2016-12-13 14:54:05 +0800

    change

--- Lm.Eic.App.Business.Bmp/Purchase/SupplierManager/SupplierTutorManger.cs
+++ Lm.Eic.App.Business.Bmp/Purchase/SupplierManager/SupplierTutorManger.cs
@@ -6,13 +6,11 @@ using Lm.Eic.Uti.Common.YleeOOMapper;
 
 namespace Lm.Eic.App.Business.Bmp.Purchase.SupplierManager
 {
- 
     /// <summary>
     /// 供应商辅导\计划管理
     /// </summary>
-public     class SupplierTutorManger
+    public class SupplierTutorManger
     {
-
         /// <summary>
         /// 得到考核分数低80分的供应商信息
         /// </summary>
@@ -24,33 +22,29 @@ public     class SupplierTutorManger
             List<SupplierSeasonAuditModel> auditNewModelLsit = new List<SupplierSeasonAuditModel>();
             //得到低于80分的所以供应商
             var auditModelLsit = SupplierCrudFactory.SuppliersSeasonAuditCrud.GetlimitScoreSupplierAuditInfo(seasonDateNum, 80);
-            if (auditModelLsit!=null&&auditModelLsit .Count >0)
+            if (auditModelLsit != null && auditModelLsit.Count > 0)
             {
                 auditModelLsit.ForEach(m =>
                 {
-                   if( SupplierCrudFactory.SuppliersSeasonTutorCrud.IsExist(m.ParameterKey))
-                    { waittingTourSupplierList.Add(SupplierCrudFactory.SuppliersSeasonTutorCrud.GetSupplierSeasonTutorModelBy(m.ParameterKey));}
-                   else
+                    if (SupplierCrudFactory.SuppliersSeasonTutorCrud.IsExist(m.ParameterKey))
+                    { waittingTourSupplierList.Add(SupplierCrudFactory.SuppliersSeasonTutorCrud.GetSupplierSeasonTutorModelBy(m.ParameterKey)); }
+                    else
                     { auditNewModelLsit.Add(m); }
                 });
             }
-            return waittingTourSupplierList.Union(OOMaper.Mapper<SupplierSeasonAuditModel, SupplierSeasonTutorModel>(auditNewModelLsit).ToList()).ToList ();
+            return waittingTourSupplierList.Union(OOMaper.Mapper<SupplierSeasonAuditModel, SupplierSeasonTutorModel>(auditNewModelLsit).ToList()).ToList();
         }
         /// <summary>
         /// 保存供应商辅导\计划管理
         /// </summary>
         /// <param name="model"></param>
         /// <returns></returns>
-        public OpResult StoreSupplierTutorModel(SupplierSeasonTutorModel model)
+        public OpResult SaveSupplierTutorModel(SupplierSeasonTutorModel model)
         {
             if (SupplierCrudFactory.SuppliersSeasonTutorCrud.IsExist(model.ParameterKey))
                 model.OpSign = "edit";
             else model.OpSign = "add";
             return SupplierCrudFactory.SuppliersSeasonTutorCrud.Store(model);
         }
-
-        
-
-
     }
 }
