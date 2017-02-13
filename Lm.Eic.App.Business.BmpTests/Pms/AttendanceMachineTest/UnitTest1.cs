eda58a0d148ed1fe319682ad05a775ed8b1ccc67
xiaoqiao <wanxiaoqiao888@163.com>
2017-02-13 11:42:59 +0800

    change

--- Lm.Eic.App.Business.BmpTests/Pms/AttendanceMachineTest/UnitTest1.cs
+++ Lm.Eic.App.Business.BmpTests/Pms/AttendanceMachineTest/UnitTest1.cs
@@ -2,6 +2,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Lm.Eic.App.Business.Attendance;
 using System.Text;
+using System.Globalization;
 
 namespace Lm.Eic.App.Business.BmpTests.Pms.AttendanceTest
 {
@@ -14,12 +15,30 @@ namespace Lm.Eic.App.Business.BmpTests.Pms.AttendanceTest
   
         public void TestMethod1()
         {
-            AttendanceBusiness attendance = new AttendanceBusiness();
-            string msg = string.Empty;
-            attendance.OpenConnent("192.168.0.24", 1, out msg);
-           var dd= attendance.GetNewAddEnrollSourceData(1);
-            if (msg == string.Empty) { Assert.Fail(); }
+            DateTime ddd = Convert .ToDateTime ("2016-01-5");
+            int ii = GetWeekOfYear(ddd);
+           // AttendanceBusiness attendance = new AttendanceBusiness();
+           // string msg = string.Empty;
+           // attendance.OpenConnent("192.168.0.24", 1, out msg);
+           //var dd= attendance.GetNewAddEnrollSourceData(1);
+           // if (msg == string.Empty) { Assert.Fail(); }
+        }
+
+        private static int GetWeekOfYear(DateTime dt)
+        {
+            GregorianCalendar gc = new GregorianCalendar();
+            int weekOfYear = gc.GetWeekOfYear(dt, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+            DateTime NewYearsDay = new DateTime(dt.Year, 1, 1);
+            //如果元旦那天刚好是星期天 没全年周次减1
+            int dayofweek = (int)NewYearsDay.DayOfWeek;
+            if (dayofweek==0)
+            {
+                weekOfYear = weekOfYear-1;
+            }
+          
+            return weekOfYear;
         }
        
+
     }
 }
