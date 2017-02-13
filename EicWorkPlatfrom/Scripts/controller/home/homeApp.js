9a61bbd8753c6add6d307e533ddda5d92e63c1d3
JustForZR <15968035606@163.com>
2017-02-13 13:15:51 +0800

    change

--- EicWorkPlatfrom/Scripts/controller/home/homeApp.js
+++ EicWorkPlatfrom/Scripts/controller/home/homeApp.js
@@ -38,8 +38,17 @@ angular.module('bpm.homeApp', ['eicomm.directive', 'ngAnimate', 'ui.router', 'ng
         },
         navLeftSize: '20%',
         navMainSize: '80%',
+        nowYear: new Date().getFullYear(),
+        nowMonth: new Date().getMonth() + 1,
+
     };
     $scope.navLayout = layoutVm;
+    //013935
+    var initVM = _.clone(layoutVm);
+    var vmManager = {
+        calendarDatas: [],
+    }
+    $scope.vmManager = vmManager;
 
     ///个人头像
     $scope.headPortrait = "../Content/login/profilepicture.jpg";
@@ -51,59 +60,48 @@ angular.module('bpm.homeApp', ['eicomm.directive', 'ngAnimate', 'ui.router', 'ng
     $scope.loadHeadPortrait();
 
     //013935创建日历视图模型
-    var calendarVm = {
-        nowYear: null,
-        nowMonth: null,
-        showEdit :false,
-    }
-    $scope.calendarVm = calendarVm;
-    nowYear = new Date().getFullYear();
-    nowMonth = new Date().getMonth() + 1;
     $scope.weeks = [5, 6, 7, 8, 9];
-    $scope.calendarDatas = [
+    vmManager.calendarDatas = [
         { week: 5 },{ week: 5 },{ week: 5 },
-        { dates: 1, day: 'wed', week: 5 ,color:'#ccc',tips:'1',calendarEdit:false},
+        { dates: 1, day: 'wed', week: 5 ,color:'#783',tips:'1',calendarEdit:false},
         { dates: 2, day: 'thu', week: 5, color: '#ccc', tips: '2',calendarEdit:false },
-        { dates: 3, day: 'fri', week: 5, color: '#ccc', tips: '3',calendarEdit:false },
-        { dates: 4, day: 'sat', week: 5 },
-        { dates: 5, day: 'sun', week: 6 },
-        { dates: 6, day: 'mon', week: 6 },
-        { dates: 7, day: 'tue', week: 6 },
-        { dates: 8, dat: 'wed', week: 6 },
-        { dates: 9, day: 'thu', week: 6 },
-        { dates: 10, day: 'fri', week: 6 },
-        { dates: 11, day: 'sat', week: 6 },
-        { dates: 12, day: 'sun', week: 7 },
-        { dates: 13, day: 'mon', week: 7 },
-        { dates: 14, day: 'tue', week: 7 },
-        { dates: 15, dat: 'wed', week: 7 },
-        { dates: 16, day: 'thu', week: 7 },
-        { dates: 17, day: 'fri', week: 7 },
-        { dates: 18, day: 'sat', week: 7 },
-        { dates: 19, day: 'sun', week: 8 },
-        { dates: 20, day: 'mon', week: 8 },
-        { dates: 21, day: 'tue', week: 8 },
-        { dates: 22, dat: 'wed', week: 8 },
-        { dates: 23, day: 'thu', week: 8 },
-        { dates: 24, day: 'fri', week: 8 },
-        { dates: 25, day: 'sat', week: 8 },
-        { dates: 26, day: 'sun', week: 9 },
-        { dates: 27, day: 'mon', week: 9 },
-        { dates: 28, day: 'tue', week: 9, type:'lastDay'},
+        { dates: 3, day: 'fri', week: 5, color: '#213', tips: '3',calendarEdit:false },
+        { dates: 4, day: 'sat', week: 5, color: '#ccc', calendarEdit: false },
+        { dates: 5, day: 'sun', week: 6, color: '#aaa', calendarEdit: false },
+        { dates: 6, day: 'mon', week: 6, color: '#bbb', calendarEdit: false },
+        { dates: 7, day: 'tue', week: 6, color: '#167', calendarEdit: false },
+        { dates: 8, dat: 'wed', week: 6,color:'#ccc', calendarEdit: false },
+        { dates: 9, day: 'thu', week: 6, color: '#ddd', calendarEdit: false },
+        { dates: 10, day: 'fri', week: 6, color: '#ccc', calendarEdit: false },
+        { dates: 11, day: 'sat', week: 6, color: '#381', calendarEdit: false },
+        { dates: 12, day: 'sun', week: 7, color: '#ccc', calendarEdit: false },
+        { dates: 13, day: 'mon', week: 7, color: '#ccc', calendarEdit: false },
+        { dates: 14, day: 'tue', week: 7, color: '#412', calendarEdit: false },
+        { dates: 15, dat: 'wed', week: 7, color: '#ccc', calendarEdit: false },
+        { dates: 16, day: 'thu', week: 7, color: '#ccc', calendarEdit: false },
+        { dates: 17, day: 'fri', week: 7, color: '#eee', calendarEdit: false },
+        { dates: 18, day: 'sat', week: 7, color: '#ccc', calendarEdit: false },
+        { dates: 19, day: 'sun', week: 8, color: '#ccc', calendarEdit: false },
+        { dates: 20, day: 'mon', week: 8,color:'#ccc', calendarEdit: false },
+        { dates: 21, day: 'tue', week: 8, color: '#ccc', calendarEdit: false },
+        { dates: 22, dat: 'wed', week: 8, color: '#212', calendarEdit: false },
+        { dates: 23, day: 'thu', week: 8, color: '#ccc', calendarEdit: false },
+        { dates: 24, day: 'fri', week: 8, color: '#cad', calendarEdit: false },
+        { dates: 25, day: 'sat', week: 8, color: '#fff', calendarEdit: false },
+        { dates: 26, day: 'sun', week: 9, color: '#ccc', calendarEdit: false },
+        { dates: 27, day: 'mon', week: 9, color: '#546', calendarEdit: false },
+        { dates: 28, day: 'tue', week: 9, color: '#ccc', calendarEdit: false },
         { week: 9 }, { week: 9 }, { week: 9 }, { week: 9 }
     ]
-    $scope.promise = homeDataopService.getCalendarDatas(nowYear,nowMonth).then(function (datas) {
+    $scope.promise = homeDataopService.getCalendarDatas(layoutVm.nowYear, layoutVm.nowMonth).then(function (datas) {
 
     });
-    $scope.calendarTipsEdit = function (item) {
-        item.calendarEdit = true;
-        console.log(item)
-    }
+    
 })
 .factory('homeDataopService', function (ajaxService) {
     var home = {};
     var calendarUrl = "/home/";
-    home.getCalendarDatas = function (nowYear, nowMonth) {
+    home.getCalendarDatas = function (nowYear,nowMonth) {
         var url = calendarUrl + "GetCalendarDatas";
         return ajaxService.getData(url, {
             nowYear: nowYear,
