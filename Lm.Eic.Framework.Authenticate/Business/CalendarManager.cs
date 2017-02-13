b1fa16cffcba9634b77452cd74bb2b48118aa39b
xiaoqiao <wanxiaoqiao888@163.com>
2017-02-13 11:45:08 +0800

    change

--- Lm.Eic.Framework.Authenticate/Business/CalendarManager.cs
+++ Lm.Eic.Framework.Authenticate/Business/CalendarManager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -13,16 +14,21 @@ namespace Lm.Eic.Framework.Authenticate.Business
             return null;
         }
 
-        public int getWeek(DateTime dtSel)
+        /// <summary>
+        /// 获取日期是当月中的第几周
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
+        private int GetDateWeekBy(DateTime dt)
         {
             bool sundayStart = true;
             //如果要判断的日期为1号，则肯定是第一周了
-            if (dtSel.Day == 1)
+            if (dt.Day == 1)
                 return 1;
             else
             {
                 //得到本月第一天
-                DateTime dtStart = new DateTime(dtSel.Year, dtSel.Month, 1);
+                DateTime dtStart = new DateTime(dt.Year, dt.Month, 1);
                 //得到本月第一天是周几
                 int dayofweek = (int)dtStart.DayOfWeek;
                 //如果不是以周日开始，需要重新计算一下dayofweek，详细DayOfWeek枚举的定义
@@ -35,17 +41,32 @@ namespace Lm.Eic.Framework.Authenticate.Business
                 //得到本月的第一周一共有几天
                 int startWeekDays = 7 - dayofweek;
                 //如果要判断的日期在第一周范围内，返回1
-                if (dtSel.Day <= startWeekDays)
+                if (dt.Day <= startWeekDays)
                     return 1;
                 else
                 {
-                    int aday = dtSel.Day + 7 - startWeekDays;
+                    int aday = dt.Day + 7 - startWeekDays;
                     return aday / 7 + (aday % 7 > 0 ? 1 : 0);
                 }
             }
         }
+        /// <summary>
+        /// 获取日期是全年中的第几周
+        /// </summary>
+        /// <param name="dt"></param>
+        /// <returns></returns>
 
-        //日期处理   
+        private static int GetWeekOfYear(DateTime dt)
+        {
+            GregorianCalendar gc = new GregorianCalendar();
+            int weekOfYear = gc.GetWeekOfYear(dt, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+            DateTime NewYearsDay = new DateTime(dt.Year, 1, 1);
+            //如果元旦那天刚好是星期天 没全年周次减1
+            if ((int)NewYearsDay.DayOfWeek == 0)
+            { weekOfYear -= 1; }
+            return weekOfYear;
+        }
+
 
     }
 }
