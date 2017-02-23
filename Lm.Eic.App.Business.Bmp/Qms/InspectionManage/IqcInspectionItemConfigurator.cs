b541acca3511ac8a44559e95ed7ee711443cef91
xiaoqiao <wanxiaoqiao888@163.com>
2017-02-23 10:03:56 +0800

    chnage

---
+++ Lm.Eic.App.Business.Bmp/Qms/InspectionManage/IqcInspectionItemConfigurator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lm.Eic.App.Business.Bmp.Qms.InspectionManage
+{
+
+    /// <summary>
+    /// IQC 进料检验配置器
+    /// </summary>
+    public class IqcInspectionItemConfigurator
+    {
+    }
+  
+}
