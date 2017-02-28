4d50a73427fe2239be56f48c277c6afbce5d52cb
xiaoqiao <wanxiaoqiao888@163.com>
2017-02-28 16:18:01 +0800

    change

--- Lm.Eic.App.Business.BmpTests/Quantity/UnitTest.cs
+++ Lm.Eic.App.Business.BmpTests/Quantity/UnitTest.cs
@@ -11,20 +11,20 @@ namespace Lm.Eic.App.Business.BmpTests.Quantity
         {
             //var n = QuantityService.IQCSampleItemsRecordManager.GetSamplePrintItemBy("32AAP00001200RM");
             //  /// 测工单从ERP中得到物料信息
-            var m=QuantityServices. SampleManger .SampleItemsIqcRecordManager.GetPuroductSupplierInfo("591-1607032");
-            var mm =QuantityServices. SampleManger.SampleItemsIqcRecordManager.GetPringSampleItemBy("591-1607032", "32AAP00001200RM");
-            var ms = QuantityServices.SampleManger.MaterialSampleItemsManager.GetMaterilalSampleItemBy("32AAP00001200RM");
-            System.IO.MemoryStream stream= QuantityServices. SampleManger.SampleItemsIqcRecordManager.ExportPrintToExcel(mm);
-           #region 输出到Excel
-           string path = @"E:\\IQC.xls";
-            using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Create, System.IO.FileAccess.Write))
-            {
-                byte[] bArr = stream.ToArray();
-                fs.Write(bArr, 0, bArr.Length);
-                fs.Flush();
+           // var m=QuantityServices. SampleManger .SampleItemsIqcRecordManager.GetPuroductSupplierInfo("591-1607032");
+           // var mm =QuantityServices. SampleManger.SampleItemsIqcRecordManager.GetPringSampleItemBy("591-1607032", "32AAP00001200RM");
+           // var ms = QuantityServices.SampleManger.MaterialSampleItemsManager.GetMaterilalSampleItemBy("32AAP00001200RM");
+           // System.IO.MemoryStream stream= QuantityServices. SampleManger.SampleItemsIqcRecordManager.ExportPrintToExcel(mm);
+           //#region 输出到Excel
+           //string path = @"E:\\IQC.xls";
+           // using (System.IO.FileStream fs = new System.IO.FileStream(path, System.IO.FileMode.Create, System.IO.FileAccess.Write))
+           // {
+           //     byte[] bArr = stream.ToArray();
+           //     fs.Write(bArr, 0, bArr.Length);
+           //     fs.Flush();
                 
-            }
-            #endregion
+           // }
+           // #endregion
 
    
            
