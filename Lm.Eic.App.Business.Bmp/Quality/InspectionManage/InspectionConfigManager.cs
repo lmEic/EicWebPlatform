e2d68fd51faf22a966fed39a1d6ca63890e639f3
eicmain <myownway888@163.com>
2017-03-06 09:43:18 +0800

    Done

--- Lm.Eic.App.Business.Bmp/Quality/InspectionManage/InspectionConfigManager.cs
+++ Lm.Eic.App.Business.Bmp/Quality/InspectionManage/InspectionConfigManager.cs
@@ -3,6 +3,7 @@ using Lm.Eic.App.Erp.Bussiness.QmsManage;
 using Lm.Eic.App.Erp.Domain.QuantityModel;
 using Lm.Eic.Uti.Common.YleeExcelHanlder;
 using Lm.Eic.Uti.Common.YleeExtension.FileOperation;
+using Lm.Eic.Uti.Common.YleeObjectBuilder;
 using Lm.Eic.Uti.Common.YleeOOMapper;
 using System;
 using System.Collections.Generic;
@@ -11,9 +12,29 @@ using System.Text;
 
 namespace Lm.Eic.App.Business.Bmp.Quality.InspectionManage
 {
+    /// <summary>
+    /// 检验配置管理器
+    /// </summary>
+    public class InspectionConfigurationManager
+    {
+        /// <summary>
+        ///检验方式配置管理器
+        /// </summary>
+        public InspectionModeConfigManager ModeConfigManager
+        {
+            get { return OBulider.BuildInstance<InspectionModeConfigManager>(); }
+        }
+        /// <summary>
+        ///Iqc检验项目配置管理器
+        /// </summary>
+        public InspectionIqcItemConfigManager IqcItemConfigManager
+        {
+            get { return OBulider.BuildInstance<InspectionIqcItemConfigManager>(); }
+        }
+    }
 
     /// <summary>
-    ///  检验方式的配置 管理器
+    ///  检验方式的配置管理器
     /// </summary>
     public class InspectionModeConfigManager
     {
@@ -24,96 +45,10 @@ namespace Lm.Eic.App.Business.Bmp.Quality.InspectionManage
         /// <returns></returns>
         public OpResult StoreInspectionModeConfig(InspectionModeConfigModel model)
         {
-            return IqcInspectionManagerCrudFactory.InspectionModeConfigCrud.Store(model, true);
+            return InspectionIqcManagerCrudFactory.InspectionModeConfigCrud.Store(model, true);
         }
     }
 
-    /// <summary>
-    /// IQC 进料检验的配置  管理器
-    /// </summary>
-    public class IqcInspectionItemConfigManager
-    {
-        /// <summary>
-        /// 物料号查询检验项目
-        /// </summary>
-        /// <param name="materialId"></param>
-        /// <returns></returns>
-        public List<IqcInspectionItemConfigModel> GetIqcspectionItemConfigDatasBy(string materialId)
-        {
-          return  IqcInspectionManagerCrudFactory.InspectionItemConfigCrud.FindIqcInspectionItemConfigDatasBy(materialId); 
-        }
-        /// <summary>
-        /// 由单号 料号 得到IQC物料检验
-        /// </summary>
-        /// <param name="orderId"></param>
-        /// <param name="materialId"></param>
-        /// <returns></returns>
-        public List<IqcInspectionMasterModel> GetIqcInspectionMasterModelListBy(string orderId,string materialId)
-        {
-            return IqcInspectionManagerCrudFactory.IqcInspectionMasterCrud.GetIqcInspectionMasterModelList(orderId, materialId);
-        }
-
-        /// <summary>
-        ///  在数据库中是否存在此料号
-        /// </summary>
-        /// <param name="materailId"></param>
-        /// <returns></returns>
-        public OpResult IsExistInspectionConfigMaterId(string materailId)
-        {
-            bool  isexixt=   IqcInspectionManagerCrudFactory.InspectionItemConfigCrud.IsExistInspectionConfigmaterailId(materailId);
-            OpResult opResult = OpResult.SetResult("", false );
-            if (isexixt) opResult = OpResult.SetResult("此物料料号已经存在", true);
-            return opResult;
-        }
-        /// <summary>
-        /// 增删改 IQC检验项目
-        /// </summary>
-        /// <param name="modelList"></param>
-        /// <returns></returns>
-        public OpResult StoreIqcInspectionItemConfig(IqcInspectionItemConfigModel model)
-        {
-           return IqcInspectionManagerCrudFactory.InspectionItemConfigCrud.Store(model,true);
-        }
-        
-        /// <summary>
-        /// 批量添加数据
-        /// </summary>
-        /// <param name="modelList"></param>
-        /// <returns></returns>
-        public OpResult StoreIqcInspectionItemConfig(List<IqcInspectionItemConfigModel> modelList)
-        {
-            return IqcInspectionManagerCrudFactory.InspectionItemConfigCrud.StoreInspectionItemConfiList(modelList);
-        }
-
-      
-
-        /// <summary>
-        /// 导入IQC 检验配置文件
-        /// </summary>
-        /// <param name="documentPatch">Excel文档路径</param>
-        /// <returns></returns>
-        public List<IqcInspectionItemConfigModel> ImportProductFlowListBy(string documentPatch)
-        {
-            StringBuilder errorStr = new StringBuilder();
-            var listEntity = ExcelHelper.ExcelToEntityList<IqcInspectionItemConfigModel>(documentPatch, out errorStr);
-            string errorStoreFilePath = @"C:\ExcelToEntity\ErrorStr.txt";
-            if (errorStr.ToString() != string.Empty)
-            {
-                errorStoreFilePath.CreateFile(errorStr.ToString());
-            }
-            return listEntity;
-        }
-
-        /// <summary>
-        /// 获取工序Excel模板
-        /// </summary>
-        /// <param name="documentPath"></param>
-        /// <returns></returns>
-        public System.IO.MemoryStream GetIqcInspectionItemConfigTemplate(string documentPath)
-        {
-            return FileOperationExtension.GetMemoryStream(documentPath);
-        }
-    }
 
 
 
