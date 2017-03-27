faafcd39d29fe13dcaa2f7b627131d391357e0d9
xiaoqiao <wanxiaoqiao888@163.com>
2017-03-27 16:10:05 +0800

    change

--- EicWorkPlatfrom/Controllers/Quality/QuaInspectionManageController.cs
+++ EicWorkPlatfrom/Controllers/Quality/QuaInspectionManageController.cs
@@ -397,9 +397,9 @@ namespace EicWorkPlatfrom.Controllers
         [NoAuthenCheck]
         public JsonResult StoreFqcInspectionGatherDatas(InspectionItemDataSummaryLabelModel gatherData)
         {
-            gatherData.DocumentPath = Path.Combine(this.CombinedFilePath(FileLibraryKey.FileLibrary, FileLibraryKey.FqcInspectionGatherDataFile, DateTime.Now.ToString("yyyyMM")), gatherData.FileName);
-            var rootPath = HttpContext.Request.PhysicalApplicationPath;
-            var datas = InspectionService.DataGatherManager.FqcDataGather.StoreFqcDataGather(gatherData,rootPath);
+            if (gatherData != null && gatherData.FileName != null)
+                gatherData.DocumentPath = Path.Combine(this.CombinedFilePath(FileLibraryKey.FileLibrary, FileLibraryKey.FqcInspectionGatherDataFile, DateTime.Now.ToString("yyyyMM")), gatherData.FileName);
+            var datas = InspectionService.DataGatherManager.FqcDataGather.StoreFqcDataGather(gatherData);
             return Json(datas);
         }
        
