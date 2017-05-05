4c22bb3418bf172759169b184024d8c495402cf3
eicmain <myownway888@163.com>
2017-05-05 13:15:12 +0800

    Done

---
+++ Lm.Eic.App.Business.Bmp/Quality/RmaMange/RamMockDatas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Lm.Eic.App.Business.Bmp.Quality.RmaMange
+{
+    public class RamMockDatas
+    {
+        //Public static List<ReportInitiateModel> ReportInitiateMockDataSet
+        //{
+        //    get
+        //    {
+        //        List<ReportInitiateModel> mockDataSet = New List<ReportInitiateModel>();
+        //        mockDataSet.Add(New ReportInitiateModel(){ RmaId = 22,CustomerShortName = 22,ProductName = 22,RmaIdStatus = 22,RmaYear = 22,RmaMonth = 22,OpPerson =,OpDate =,OpTime =,OpSign =,Id_Key =});
+        //        mockDataSet.Add(New ReportInitiateModel(){ RmaId = 22,CustomerShortName = 22,ProductName = 22,RmaIdStatus = 22,RmaYear = 22,RmaMonth = 22,OpPerson =,OpDate =,OpTime =,OpSign =,Id_Key =});
+        //        return mockDataSet;
+        //    }
+        //}
+    }
+}
