3e73a209a3d1c32e9456b75cd585d934cfb54397
eicmain <myownway888@163.com>
2017-05-22 11:35:51 +0800

    Done

--- EicWorkPlatfrom/Scripts/controller/toolsOnLine/tolOfficeAssistant.js
+++ EicWorkPlatfrom/Scripts/controller/toolsOnLine/tolOfficeAssistant.js
@@ -1,15 +1,14 @@
 ﻿/// <reference path="../../common/angulee.js" />
 /// <reference path="../../angular.min.js" />
-var officeAssistantModule = angular.module('bpm.sysmanageApp');
+var officeAssistantModule = angular.module('bpm.toolsOnlineApp');
 officeAssistantModule.factory('oAssistantDataOpService', function (ajaxService) {
     var oAssistant = {};
-    var urlPrefix = '/' + leeHelper.controllers.TolOfficeAssistant + '/';
+    var oaUrlPrefix = '/' + leeHelper.controllers.TolOfficeAssistant + '/';
 
-    ///存储项目开发记录
-    oAssistant.storeProjectDevelopRecord = function (entity) {
-        var url = urlPrefix + 'StoreProjectDevelopRecord';
-        return ajaxService.postData(url, {
-            entity: entity
+    ///联系人数据
+    oAssistant.getCollaborateContactDatas = function () {
+        var url = oaUrlPrefix + 'GetCollaborateContactDatas';
+        return ajaxService.getData(url, {
         });
     };
 
@@ -19,6 +18,65 @@ officeAssistantModule.factory('oAssistantDataOpService', function (ajaxService) 
 });
 ///名片夹控制器
 officeAssistantModule.controller('collaborateContactLibCtrl', function ($scope, oAssistantDataOpService) {
+    ///联系人模型
+    var uiVm = $scope.vm = {
+        Department: null,
+        ContactPerson: null,
+        Sex: null,
+        CustomerCategory: null,
+        ContactCompany: null,
+        WorkerPosition: null,
+        ContactMemo: null,
+        Telephone: null,
+        OfficeTelephone: null,
+        Fax: null,
+        Mail: null,
+        QqOrSkype: null,
+        ContactAdress: null,
+        WebsiteAdress: null,
+        OpPerson: null,
+        OpDate: null,
+        OpTime: null,
+        OpSign: leeDataHandler.dataOpMode.add,
+        Id_Key: null,
+    };
+    var initVm = _.clone(uiVm);
+
+    var dialog = $scope.dialog = Object.create(leeDialog);
+
+    var qryManager = $scope.qryManager = {
+        contactPerson: null,
+        telephone: null,
+    };
+
+
+    var vmManager = {
+        activeTab: 'initTab',
+        editDatas: [],
+        loadDatas: function () {
+            vmManager.editDatas = [];
+            $scope.searchPromise = oAssistantDataOpService.getCollaborateContactDatas().then(function (datas) {
+                if (angular.isArray(datas))
+                    vmManager.editDatas = datas;
+            });
+        }
+    };
+    $scope.vmManager = vmManager;
+    var operate = Object.create(leeDataHandler.operateStatus);
+    $scope.operate = operate;
+    operate.createNew = function () {
+        $scope.vm = uiVm = _.clone(initVm);
+        dialog.show();
+    };
+    operate.editItem = function (item) {
+        item.OpSign = leeDataHandler.dataOpMode.edit;
+        $scope.vm = uiVm = item;
+        dialog.show();
+    },
+    operate.saveAll = function (isValid) { };
+    operate.refresh = function () { };
+
+    vmManager.loadDatas();
 });
 ///工作任务管理控制器
 officeAssistantModule.controller('workTaskManageCtrl', function ($scope, oAssistantDataOpService) {
