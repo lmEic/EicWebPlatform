73b663c9b7b848fc68684053f1ef0003c497310a
helloworld-EZ <408457328@qq.com>
2017-05-26 16:19:03 +0800

    done

--- Lm.Eic.Framework.ProductMaster/Business/Tools/tlOnline/ToolOnlineService.cs
+++ Lm.Eic.Framework.ProductMaster/Business/Tools/tlOnline/ToolOnlineService.cs
@@ -11,8 +11,20 @@ namespace Lm.Eic.Framework.ProductMaster.Business.Tools.tlOnline
     /// </summary>
     public class ToolOnlineService
     {
-       
+        /// <summary>
+        /// 
+        /// </summary>
+        public static CollaborateContactManager ContactManager
+        {
+            get { return OBulider.BuildInstance<CollaborateContactManager>(); }
+        }
 
-
+        /// <summary>
+        /// 工作任务管理器
+        /// </summary>
+        public static WorkTaskManager WorkTaskManage
+        {
+            get { return OBulider.BuildInstance<WorkTaskManager>(); }
+        }
     }
 }
