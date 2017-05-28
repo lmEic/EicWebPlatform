1c5a04a3152c8a2dd14e8d2c1bd451a598b8e6bb
eicmain <myownway888@163.com>
2017-05-28 11:30:59 +0800

    done

--- Lm.Eic.Uti.Common/Lm.Eic.Uti.Common/YleeOOMapper/OpResult.cs
+++ Lm.Eic.Uti.Common/Lm.Eic.Uti.Common/YleeOOMapper/OpResult.cs
@@ -142,7 +142,7 @@ namespace Lm.Eic.Uti.Common.YleeOOMapper
             opResult.LogErrorMsgToFile(ex);
             return opResult;
         }
-       
+
         /// <summary>
         /// 设定操作结果
         /// </summary>
@@ -181,7 +181,6 @@ namespace Lm.Eic.Uti.Common.YleeOOMapper
             return new OpResult(successMessage, falseMessage, result);
         }
     }
-
     /// <summary>
     /// 数据操作类型
     /// </summary>
@@ -194,14 +193,10 @@ namespace Lm.Eic.Uti.Common.YleeOOMapper
         public const string UploadFile = "uploadFile";
         public const string DeleteFile = "deleteFile";
     }
-    public class DataList<T> : List<T>
-    {
-        public string ExceptionId { get; set; }
-    }
     /// <summary>
     /// OpResult的扩展方法集合
     /// </summary>
-    public static class OpResultExtension
+    public static class DataOpResultExtension
     {
         /// <summary>
         /// 扩展方法，返回异常结果
@@ -211,18 +206,6 @@ namespace Lm.Eic.Uti.Common.YleeOOMapper
         public static OpResult ExOpResult(this Exception ex)
         {
             return OpResult.SetErrorResult(ex);
-        }
-        /// <summary>
-        /// 将异常信息记录到文件中去
-        /// </summary>
-        /// <param name="ex"></param>
-        /// <param name="fnName"></param>
-        public static DataList<T> ExDataList<T>(this Exception ex)
-        {
-            OpResult result = ex.ExOpResult();
-            var datas= new DataList<T>();
-            datas.ExceptionId = result.ExceptionId;
-            return datas;
         }
     }
 }
