fe05aafee0c36b3a498a481ab417c01e97e7eacc
xiaoqiao <wanxiaoqiao888@163.com>
2017-06-20 18:23:45 +0800

    change

--- EicWorkPlatfrom/Global.asax.cs
+++ EicWorkPlatfrom/Global.asax.cs
@@ -21,7 +21,6 @@ namespace EicWorkPlatfrom
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
-
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
