52a361484b0b48313e454d6e6d89eff4955a8475
xiaoqiao <wanxiaoqiao888@163.com>
2017-07-10 09:53:09 +0800

    change

--- Lm.Eic.App.Business.BmpTests/Pms/AttendanceMachineTest/UnitTest1.cs
+++ Lm.Eic.App.Business.BmpTests/Pms/AttendanceMachineTest/UnitTest1.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Lm.Eic.App.Business.Attendance;
+
 using System.Text;
 using System.Globalization;
 
@@ -9,19 +9,19 @@ namespace Lm.Eic.App.Business.BmpTests.Pms.AttendanceTest
     [TestClass]
     public class UnitTest1
     {
-       
+
         [TestMethod]
-      
-  
+
+
         public void TestMethod1()
         {
-            DateTime ddd = Convert .ToDateTime ("2016-01-5");
+            DateTime ddd = Convert.ToDateTime("2016-01-5");
             int ii = GetWeekOfYear(ddd);
-           // AttendanceBusiness attendance = new AttendanceBusiness();
-           // string msg = string.Empty;
-           // attendance.OpenConnent("192.168.0.24", 1, out msg);
-           //var dd= attendance.GetNewAddEnrollSourceData(1);
-           // if (msg == string.Empty) { Assert.Fail(); }
+            // AttendanceBusiness attendance = new AttendanceBusiness();
+            // string msg = string.Empty;
+            // attendance.OpenConnent("192.168.0.24", 1, out msg);
+            //var dd= attendance.GetNewAddEnrollSourceData(1);
+            // if (msg == string.Empty) { Assert.Fail(); }
         }
 
         private static int GetWeekOfYear(DateTime dt)
@@ -31,14 +31,14 @@ namespace Lm.Eic.App.Business.BmpTests.Pms.AttendanceTest
             DateTime NewYearsDay = new DateTime(dt.Year, 1, 1);
             //如果元旦那天刚好是星期天 没全年周次减1
             int dayofweek = (int)NewYearsDay.DayOfWeek;
-            if (dayofweek==0)
+            if (dayofweek == 0)
             {
-                weekOfYear = weekOfYear-1;
+                weekOfYear = weekOfYear - 1;
             }
-          
+
             return weekOfYear;
         }
-       
+
 
     }
 }
