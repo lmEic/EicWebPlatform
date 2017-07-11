7479f4fd8c25349863b0dae42b214e45da5a1e9b
林旺雷 <linwanglei@hotmail.com>
2017-07-11 09:56:23 +0800

    done

--- EicWorkPlatfrom/Scripts/controller/toolsOnLine/toolsOnlineApp.js
+++ EicWorkPlatfrom/Scripts/controller/toolsOnLine/toolsOnlineApp.js
@@ -14,7 +14,7 @@ angular.module('bpm.toolsOnlineApp', ['eicomm.directive', 'mp.configApp', 'ngAni
     $stateProvider.state('workTaskManage', {
         templateUrl: oAssistantUrlPrefix + 'WorkTaskManage'
     });
-    ///工作任务管理
+    ///上报改善问题
     $stateProvider.state('reportImproveProblem', {
         templateUrl: oAssistantUrlPrefix + 'ReportImproveProblem'
     });
