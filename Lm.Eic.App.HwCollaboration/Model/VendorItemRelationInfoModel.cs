3e66da6ba22c6bcc120b51af3946e328f37848fe
eicmain <myownway888@163.com>
2017-08-22 16:34:35 +0800

    Done

---
+++ Lm.Eic.App.HwCollaboration/Model/VendorItemRelationInfoModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lm.Eic.App.HwCollaboration.Model
+{
+    /// <summary>
+    /// 物料基础信息模型
+    /// </summary>
+    [Serializable]
+    public class VendorItemRelationInfoModel
+    {
+        /// <summary>
+        /// 供应商代码
+        /// </summary>
+        public string VendorCode { get; set; }
+        /// <summary>
+        /// 供应商名称
+        /// </summary>
+        public string VendorName { get; set; }
+        /// <summary>
+        /// 供应商物料编码
+        /// </summary>
+        public string VendorItemCode { get; set; }
+        /// <summary>
+        /// 供应商产品型号
+        /// </summary>
+        public string VendorItemType { get; set; }
+        /// <summary>
+        /// 供应商物料描述
+        /// </summary>
+        public string VendorItemDesc { get; set; }
+        /// <summary>
+        /// 物料小类
+        /// </summary>
+        public string ItemCategory { get; set; }
+        /// <summary>
+        /// 客户代码
+        /// </summary>
+        public string CustomerCodeHW { get; set; }
+        /// <summary>
+        /// 客户名称
+        /// </summary>
+        public string CustomerName { get; set; }
+        /// <summary>
+        /// 客户物料编码
+        /// </summary>
+        public string CustomerItemCode { get; set; }
+        /// <summary>
+        /// 客户产品型号
+        /// </summary>
+        public string CustomerItemType { get; set; }
+        /// <summary>
+        /// 单位
+        /// </summary>
+        public string Unit { get; set; }
+        /// <summary>
+        /// 供应商Item类别
+        /// </summary>
+        public string VendorInventoryType { get; set; }
+        /// <summary>
+        /// 良率%
+        /// </summary>
+        public float YieldRate { get; set; }
+        /// <summary>
+        /// 货期(天）
+        /// </summary>
+        public int LeadTime { get; set; }
+        /// <summary>
+        /// 生命周期状态
+        /// </summary>
+        public string Item_cycle_Status { get; set; }
+    }
+}
---
+++ Lm.Eic.App.HwCollaboration/Model/VendorTaskSendMaterialDetailModel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lm.Eic.App.HwCollaboration.Model
+{
+    /// <summary>
+    ///供应商任务令发料明细模型
+    /// </summary>
+    [Serializable]
+    public class VendorTaskSendMaterialDetailModel
+    {
+        /// <summary>
+        /// 供应商代码
+        /// </summary>
+        public string VendorCode { get; set; }
+        /// <summary>
+        /// 供应商名称
+        /// </summary>
+        public string VendorName { get; set; }
+        /// <summary>
+        /// 工厂代码
+        /// </summary>
+        public string VendorFactoryCode { get; set; }
+
+        /// <summary>
+        /// 生产订单号
+        /// ORDER_NUMBER
+        /// </summary>
+        public string OrderNumber { get; set; }
+        /// <summary>
+        /// 客户代码
+        /// </summary>
+        public string CustomerCode { get; set; }
+
+        /// <summary>
+        /// 生产待发物料编码
+        /// PRO_DUE_OUT_MATERIAL_CODE
+        /// 供应商生产订单的待发物料编码
+        /// </summary>
+        public string ProDueOutMaterialCode { get; set; }
+        /// <summary>
+        /// 生产应发数量
+        /// PRO_SHOULD_SEND_QUANTITY
+        /// 供应商生产订单需求的物料编码的应发数量
+        /// </summary>
+        public int ProShouldSendQuantity { get; set; }
+        /// <summary>
+        /// 生产已发数量
+        /// PRO_HAS_SENT_QUANTITY
+        /// 供应商生产订单需求的物料编码的已发数量
+        /// </summary>
+        public int ProHasSentQuantity { get; set; }
+        /// <summary>
+        /// BOM用量
+        /// BOM_AMOUNT_USED
+        /// 供应商生产订单需求的物料编码的BOM用量
+        /// </summary>
+        public int BomAmountUsed { get; set; }
+        /// <summary>
+        /// 替代组
+        /// SUBSTITUTION_GROUP
+        /// 替代料组代码
+        /// </summary>
+        public string SubstitutionGroup { get; set; }
+    }
+}
