d1e6f2fb2ef7116e5544139ce67eeadb75a1b42f
xiaoqiao <wanxiaoqiao888@163.com>
2017-08-31 08:03:05 +0800

    change

--- EicWorkPlatfrom/Scripts/controller/quality/qua8DManage.js
+++ EicWorkPlatfrom/Scripts/controller/quality/qua8DManage.js
@@ -279,9 +279,7 @@ qualityModule.controller('create8DFormCtrl', function ($scope, BDataOpService, d
 
 ////处理8D表单
 qualityModule.controller('Handle8DFormCtrl', function ($scope, dataDicConfigTreeSet, connDataOpService, BDataOpService) {
-
-    // var ue = leeUeditor.getEditor('stepHandleContent');
-    var editor = leeUeditor.createEditor('formContent');
+    var editor = leeUeditor.createEditor('form8DHandelData');
     ///视图模型
     var uiVm = $scope.vm = {
         ReportId: null,
@@ -349,20 +347,7 @@ qualityModule.controller('Handle8DFormCtrl', function ($scope, dataDicConfigTree
                 vmManager.isShowMasterData = true;
             });
         },
-        //根据角色选择参与人员
-        showParticipantView: function (role) {
-            vmManager.dataset = [];
-            vmManager.currentParticipantRole = role;
-            dialog.show();
-        },
-        //选择参与人
-        selectParticipant: function (participant) {
-            participant.IsChecked = !participant.IsChecked;
-            if (!participant.IsChecked) return;
-            participant.Role = vmManager.currentParticipantRole;
-            leeWorkFlow.addParticipant(vmManager.participants, participant);
-            participantInfo[vmManager.currentParticipantRole] = leeWorkFlow.getParticipantMappedRole(vmManager.participants, vmManager.currentParticipantRole);
-        },
+
         //选择步骤
         selectStep: function (step) {
             vmManager.selectStepItemData = step;
@@ -398,7 +383,23 @@ qualityModule.controller('Handle8DFormCtrl', function ($scope, dataDicConfigTree
         showMasterData: function () {
             if (vmManager.isShowMasterData) vmManager.isShowMasterData = false;
             else vmManager.isShowMasterData = true;
-        }
+        },
+
+        //根据角色选择参与人员
+        showParticipantView: function (role) {
+            vmManager.dataset = [];
+            vmManager.currentParticipantRole = role;
+            dialog.show();
+        },
+        //选择参与人
+        selectParticipant: function (participant) {
+            participant.IsChecked = !participant.IsChecked;
+            if (!participant.IsChecked) return;
+            participant.Role = vmManager.currentParticipantRole;
+            leeWorkFlow.addParticipant(vmManager.participants, participant);
+            participantInfo[vmManager.currentParticipantRole] = leeWorkFlow.getParticipantMappedRole(vmManager.participants, vmManager.currentParticipantRole);
+        },
+
     };
     $scope.vmManager = vmManager;
     //上传文件
@@ -446,6 +447,25 @@ qualityModule.controller('Handle8DFormCtrl', function ($scope, dataDicConfigTree
         });
     }
 
+
+    $scope.promise = connDataOpService.getConfigDicData('Organization').then(function (datas) {
+        departmentTreeSet.setTreeDataset(datas);
+        var user = leeLoginUser;
+        participantInfo[leeWorkFlow.participantRole.Applicant] = leeWorkFlow.toParticipant(user);
+    });
+    var departmentTreeSet = dataDicConfigTreeSet.getTreeSet('departmentTree', "组织架构");
+    departmentTreeSet.bindNodeToVm = function () {
+        var dto = _.clone(departmentTreeSet.treeNode.vm);
+        var department = dto.DataNodeText;
+        vmManager.dataset = [];
+        $scope.searchPromise = wfDataOpService.getWorkerMails(department).then(function (datas) {
+            angular.forEach(datas, function (dataItem) {
+                var item = _.clone(workerEmailInfo);
+                leeHelper.copyVm(dataItem, item);
+                vmManager.dataset.push(item);
+            });
+        });
+    };
     //vmManager.query8DStepDatas();
 });
 
