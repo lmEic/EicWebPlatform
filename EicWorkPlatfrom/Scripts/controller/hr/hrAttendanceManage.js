8dd6ef11b393ddef08f61d9d2d8696dca7d5d53e
林旺雷 <linwanglei@hotmail.com>
2017-09-15 16:28:24 +0800

    change

--- EicWorkPlatfrom/Scripts/controller/hr/hrAttendanceManage.js
+++ EicWorkPlatfrom/Scripts/controller/hr/hrAttendanceManage.js
@@ -624,15 +624,23 @@ hrModule.controller('workOverHoursManageCtrl', function ($scope, $modal, hrDataO
             vmManager.dataSets.push(vm);
         },
         //删除行
-        removeRow: function (item) {
+        removeRow: function (item) {         
             leeHelper.remove(vmManager.dataSets, item);
+            var rowindex = item.rowindex;
+            vmManager.edittingRowIndex = rowindex - 1;
+            var index = 1;
+            //重新更改行的索引
+            angular.forEach(vmManager.dataSets, function (row) {
+                row.rowindex = index;
+                index += 1;
+            }),   
+            alert(item.rowindex);
             tempVm.tabCount = vmManager.dataSets.length;
             //累计时数
             tempVm.workOverCount = 0;
             angular.forEach(vmManager.dataSets, function (row) {
                 $scope.tempVm.workOverCount += row.WorkOverHours;
             })
-
         },
         //复制粘贴行
         copyAndPaste: function (item) {             
@@ -834,7 +842,8 @@ hrModule.controller('workOverHoursManageCtrl', function ($scope, $modal, hrDataO
         },     
         //加班时数输入框
         inputWorkOverHours: function ($event, item)
-        {        
+        {  
+           
             var year = new Date().getFullYear();
             var mm = new Date().getMonth() + 1;               
             var dd = new Date().getDate();
