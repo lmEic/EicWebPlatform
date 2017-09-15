21b5d06e857e73255901a8423ed7c454dd05413e
eicmain <myownway888@163.com>
2017-09-15 09:31:05 +0800

    Done

--- Lm.Eic.App.HwCollaboration/DbAccess/HwDatasTransferDb.cs
+++ Lm.Eic.App.HwCollaboration/DbAccess/HwDatasTransferDb.cs
@@ -30,15 +30,15 @@ namespace Lm.Eic.App.HwCollaboration.DbAccess
         public HwCollaborationDataTransferModel GetLatestDataModel(string moduleName)
         {
             StringBuilder sbSql = new StringBuilder();
-            sbSql.Append("select Top 1 OpModule, OpContent, OpDate, OpTime, OpPerson, OpSign from HwCollaboration_DataTransfer")
+            sbSql.Append("select Top 1 OpModule, OpContent,OpLog, OpDate, OpTime, OpPerson, OpSign from HwCollaboration_DataTransfer")
                 .AppendFormat(" where OpModule='{0}'", moduleName);
             return DbHelper.Bpm.LoadEntity<HwCollaborationDataTransferModel>(sbSql.ToString());
         }
         private OpResult Insert(HwCollaborationDataTransferModel entity)
         {
             StringBuilder sqlSb = new StringBuilder();
-            sqlSb.Append("Insert into HwCollaboration_DataTransfer(OpModule, OpContent, OpDate, OpTime, OpPerson, OpSign)")
-                 .AppendFormat("values('{0}','{1}','{2}','{3}','{4}','{5}')", entity.OpModule, entity.OpContent, entity.OpDate, entity.OpTime, entity.OpPerson, entity.OpSign);
+            sqlSb.Append("Insert into HwCollaboration_DataTransfer(OpModule, OpContent,OpLog,OpDate, OpTime, OpPerson, OpSign)")
+                 .AppendFormat("values('{0}','{1}','{2}','{3}','{4}','{5}','{6}')", entity.OpModule, entity.OpContent, entity.OpLog, entity.OpDate, entity.OpTime, entity.OpPerson, entity.OpSign);
             int record = DbHelper.Bpm.ExecuteNonQuery(sqlSb.ToString());
             if (record > 0)
                 return OpResult.SetSuccessResult("向华为协同平台发送数据成功！");
