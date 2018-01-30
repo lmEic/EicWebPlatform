86f078d80109235accae958ced4228aa09792872
linwanglei <linwanglei@hotmail.com>
2018-01-30 13:39:23 +0800

    change

--- Lm.Eic.App.DbAccess.Bpm/Repository/AstRep/EquipmentRepository.cs
+++ Lm.Eic.App.DbAccess.Bpm/Repository/AstRep/EquipmentRepository.cs
@@ -29,7 +29,6 @@ namespace Lm.Eic.App.DbAccess.Bpm.Repository.AstRep
     public class EquipmentMaintenanceRepository : BpmRepositoryBase<EquipmentMaintenanceRecordModel>, IEquipmentMaintenanceRepositor
     {
     }
-
     /// <summary>
     ///设备报废 仓储层
     /// </summary>
